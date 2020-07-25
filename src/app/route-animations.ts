526dce902c7f18860190c5aebd2c5416aa95df61
rajesh1420621 <panigrahirajesh578@gmail.com>
2020-07-25 15:28:33 +0530

    Test message 1

---
+++ src/app/route-animations.ts
@@ -0,0 +1,83 @@
+import {
+  transition,
+  trigger,
+  query,
+  style,
+  animate,
+  group,
+  animateChild
+} from '@angular/animations';
+export const slideInAnimation =
+  trigger('routeAnimations', [
+       transition('register => *', [
+            query(':enter, :leave', 
+                 style({ position: 'fixed', width: '100%' }), 
+                 { optional: true }),        
+            group([
+                 query(':enter',[
+                     style({ transform: 'translateX(-100%)' }),
+                     animate('0.7s ease-in-out', 
+                     style({ transform: 'translateX(0%)' }))
+                 ], { optional: true }),
+                 query(':leave', [
+                     style({ transform:   'translateX(0%)'}),
+                     animate('0.7s ease-in-out', 
+                     style({ transform: 'translateX(100%)' }))
+                 ], { optional: true }),
+            ])
+       ]),
+       transition('getallemoloyess => *', [
+            query(':enter, :leave', 
+                 style({ position: 'fixed',  width: '100%' }), 
+                 { optional: true }),
+            group([
+                 query(':enter', [
+                     style({ transform: 'translateX(100%)' }), 
+                     animate('0.7s ease-in-out', 
+                     style({ transform: 'translateX(0%)' }))
+                 ], { optional: true }),
+                 query(':leave', [
+                     style({ transform: 'translateX(0%)' }),
+                     animate('0.7s ease-in-out', 
+                     style({ transform: 'translateX(-100%)' }))
+                     ], { optional: true }),
+             ])
+       ]),
+         
+      
+       transition('home => getallemoloyess', [
+             query(':enter, :leave', 
+                 style({ position: 'fixed', width: '100%' }), 
+                 { optional: true }),
+             group([
+                 query(':enter', [
+                     style({ transform: 'translateX(100%)' }),
+                     animate('0.5s ease-in-out', 
+                     style({ transform: 'translateX(0%)' }))
+                 ], { optional: true }),
+                 query(':leave', [
+                     style({ transform: 'translateX(0%)' }),
+                     animate('0.5s ease-in-out', 
+                     style({ transform: 'translateX(-100%)' }))
+                 ], { optional: true }),
+             ])
+       ]),
+       transition('home => register', [
+             query(':enter, :leave', 
+                 style({ position: 'fixed', width: '100%' }), 
+                 { optional: true }),
+             group([
+                 query(':enter', [
+                     style({ transform: 'translateX(-100%)' }),
+                     animate('0.5s ease-in-out', 
+                     style({ transform: 'translateX(0%)' }))
+                 ], { optional: true }),
+                 query(':leave', [
+                      style({ transform: 'translateX(0%)' }),
+                      animate('0.5s ease-in-out', 
+                      style({ transform: 'translateX(100%)' }))
+                 ], { optional: true }),
+             ])
+      ]),
+      
+]);
